f308f5e26ecf4462f532d9642171d616db27a97f
Roger Garcia <rogergarciaviles@gmail.com>
2025-01-10 14:20:09 +0100

    Added patrol enemy test

--- PRACTICA1_Unity/Assets/Enemies/Scripts/RotatingEnemy.cs
+++ PRACTICA1_Unity/Assets/Enemies/Scripts/RotatingEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class RotatingEnemy : MonoBehaviour
 {
     public void rotateEnemy()
     {
