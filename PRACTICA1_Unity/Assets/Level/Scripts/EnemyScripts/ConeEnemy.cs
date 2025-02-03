a2d1dcb6fab9a4bda858307e828dbe4f20083d8b
Roger Garcia <rogergarciaviles@gmail.com>
2025-02-03 12:33:31 +0100

    little changes lvl3

---
+++ PRACTICA1_Unity/Assets/Level/Scripts/EnemyScripts/ConeEnemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ConeEnemy : MonoBehaviour
+{
+    private bool turnUp;
+    private bool turnDown;
+    private void Update()
+    {
+        if (turnUp)
+        {
+            if (transform.localRotation.z > 15f)
+            {
+                turnUp = false;
+                turnDown = true;
+            }
+            else
+            {
+                transform.Rotate(0, 0, 5f * Time.deltaTime);
+            }    
+        }
+        if (turnDown)
+        {
+            if (transform.localRotation.z < -15f)
+            {
+                turnUp = true;
+                turnDown = false;
+            }
+            else
+            {
+                transform.Rotate(0, 0, -5f * Time.deltaTime);
+            }
+        }
+    }
+
+}
