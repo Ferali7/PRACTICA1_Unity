6cfc5bcef5a75c93714d30985246c21f5ba12b02
Roger Garcia <rogergarciaviles@gmail.com>
2025-02-03 13:18:01 +0100

    idk

--- PRACTICA1_Unity/Assets/Level/Scripts/EnemyScripts/ConeEnemy.cs
+++ PRACTICA1_Unity/Assets/Level/Scripts/EnemyScripts/ConeEnemy.cs
@@ -5,7 +5,7 @@ using UnityEngine.Rendering.Universal;
 
 public class ConeEnemy : MonoBehaviour
 {
-    private bool turnUp = true;
+    private bool turnUp;
     private bool turnDown;
     private void Update()
     {
