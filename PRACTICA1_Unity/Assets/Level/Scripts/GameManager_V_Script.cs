c8f99a2b6c8c91baff57d8e69a1db5f283c719ec
Ferali7 <aliagarodriguezferran@gmail.com>
2025-02-04 20:47:32 +0100

    roger

--- PRACTICA1_Unity/Assets/Level/Scripts/GameManager_V_Script.cs
+++ PRACTICA1_Unity/Assets/Level/Scripts/GameManager_V_Script.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager_V_Script : MonoBehaviour
 {
+    //script que controla totes les variables que no es ressetegen al començar cada nivell, sinó que es mantenen fins al final
     public int numberKeys = 0;
     public int amountMoney = 0;
     public int amountMoneyLv1 = 0;
@@ -11,8 +12,10 @@ public class GameManager_V_Script : MonoBehaviour
     public int amountMoneyLv3 = 0;
     public string currentLevel;
     public static GameManager_V_Script instance;
+    //ho posem en awake, que passa abans del Start
     private void Awake()
     {
+        ////si el GameManager_Variables no ha ocorregut en cap instancia, aquest script fa que la instància principal sigui aquesta, i si ja hi ha una instància, no assignarà una nova com a instància, sinó que destruirà la nova. això preveu duplicats de la instància
         if (instance == null)
         {
             instance = this;
@@ -28,14 +31,13 @@ public class GameManager_V_Script : MonoBehaviour
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         currentLevel = SceneManager.GetActiveScene().name;
         UpdateMoney();
     }
-    //funcions per controlar les variables
+    //funcions per controlar el augment de variables
     public void AddKey()
     {
         numberKeys++;
@@ -49,6 +51,7 @@ public class GameManager_V_Script : MonoBehaviour
         int randomMoney = Random.Range(300, 500);
         amountMoney = amountMoney + randomMoney;
     }
+    //funció que utilitzem, sobretot, per quan fem restart al morir en un nivell. A cada nivell que estem, es guarda el valor de diners actual en amountMoney(Lv actual). Al fer resset, carregarem els diners finals obtinguts en el nivell anterior, per començar el nivell de nou sense els diners que hem aconseguit en el mateix abans de morir.
     public void UpdateMoney()
     {
         if (currentLevel == "LEVEL 1")
--- PRACTICA1_Unity/Assets/Level/Scripts/UI_Scripts/UI_Permanent.cs
+++ PRACTICA1_Unity/Assets/Level/Scripts/UI_Scripts/UI_Permanent.cs
@@ -7,29 +7,12 @@ using UnityEngine.UI;
 public class UI_Permanent : MonoBehaviour
 {
     public GameManager_V_Script gameManagerV;
-    //public GameManager_Script gameManager;
     public GameObject PauseMenu;
     public GameObject SettingsMenu;
     public TextMeshProUGUI moneyText;
     public TextMeshProUGUI keysText;
     public TextMeshProUGUI levelText;
-    //Aquest script serveix únicament per fer que la UI no es borri ni perdi dades al passar de nivell a nivell (o de pis en pis). Volem que el jugador segueixi tenint la mateixa quantitat de diners al passar d'un a altre i no s'inicialitzi.
-    public static UI_Permanent instance;
-    //aquesta funció Awake, que passa abans que carregui el nivell i abans que void Start()
-    private void Awake()
-    {
-        //si el UI no ha ocorregut en cap instancia, aquest script fa que la instància principal sigui aquesta, i si ja hi ha una instància, no assignarà una nova com a instància, sinó que destruirà la nova. això preveu duplicats de la instància.
-        /*if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }*/
-    }
-
+    
     // Start is called before the first frame update
     void Start()
     {
