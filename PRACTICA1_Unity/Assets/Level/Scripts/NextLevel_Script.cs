3166da5640fc508dded8a369f08ff2998256af58
RobertoHanma <robertohanma@gmail.com>
2025-02-05 00:23:02 +0100

    FadeInFadeOut

--- PRACTICA1_Unity/Assets/Level/Scripts/NextLevel_Script.cs
+++ PRACTICA1_Unity/Assets/Level/Scripts/NextLevel_Script.cs
@@ -6,13 +6,16 @@ using UnityEngine.SceneManagement;
 public class NextLevel_Script : MonoBehaviour
 {
     public GameManager_Script GameManager;
+    public Animator animator;
 
     IEnumerator SceneChanger()
     {
-
+        FadeToLevel();
         yield return new WaitForSeconds(2f);
         loadNextLevel();
+
     }
+    
     void loadNextLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -23,19 +26,33 @@ public class NextLevel_Script : MonoBehaviour
     {
         GameManager = GameObject.FindObjectOfType<GameManager_Script>();
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void FadeToLevel()
+    {
+
+        animator.SetTrigger("FadeOut");
+
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         //si el objecte que fa collide amb el trigger de Next Level es el "Player", va al nivell de Index+1
         if (collision.CompareTag("Player"))
         {
-           
+            //loadNextLevel();
             StartCoroutine(SceneChanger());
+            FadeToLevel();
 
         }
     }
